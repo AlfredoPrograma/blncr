14fbb70627b097edd21fcae679464a0604017326
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-04-06 20:46:44 -0400

    feature: set up base http listener

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	l, err := net.Listen("tcp4", ":9090")
+
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+
+	http.Serve(l, nil)
 }
